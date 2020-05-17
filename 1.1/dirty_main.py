202c9f1093b43715e5e2ee10639fd2f1d761c428
Anvartdinov Timur <AnvartdinovTimurLinux@gmail.com>
2020-05-17 14:31:24 +0300

    Правка замечаний 2

--- 1.1/dirty_main.py
+++ 1.1/dirty_main.py
@@ -1,2 +1,15 @@
+import time
+from datetime import datetime
+
 from application.db.people import *
 from application.salary import *
+
+
+if __name__ == '__main__':
+    print(datetime.now())
+    calculate_salary()
+
+    time.sleep(1)
+
+    print(datetime.now())
+    get_employees()
