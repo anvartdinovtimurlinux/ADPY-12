5774ced0a384eaeabaa2ff15d900c37fe921403e
Anvartdinov Timur <AnvartdinovTimurLinux@gmail.com>
2020-05-25 12:02:47 +0300

    Домашнее задание к лекции 1.4 «Decorators» Применение декораторов

--- 1.4/main.py
+++ 1.4/main.py
@@ -1,22 +1,29 @@
-import loggers.logger_without_params as logger1
-import loggers.logger_with_params as logger2
+import hashlib
+from loggers.logger_without_params import logger_without_params as logger1
+from loggers.logger_with_params import logger_with_params as logger2
 
 
-@logger1.logger_without_params
-def summator(*args):
-    return sum(args)
+@logger1
+def md5_generator(path_to_file,):
+    with open(path_to_file, 'r', encoding='utf-8') as f:
+        for line in f:
+            result = hashlib.md5(line.encode())
+            yield result.hexdigest()
 
-@logger2.logger_with_params('log2.txt')
-def summator2(*args):
-    return sum(args)
+
+@logger2('log2.txt')
+def sha256_generator(path_to_file,):
+    with open(path_to_file, 'r', encoding='utf-8') as f:
+        for line in f:
+            result = hashlib.sha256(line.encode())
+            yield result.hexdigest()
 
 
 if __name__ == '__main__':
-    print(summator(1, 2, 3, 4))
-    print(summator(1, 2, 3, 4, 5))
-    print(summator(1, 2, 3, 4, 5, 6))
+    print('Результат работы алгоритма MD5:')
+    for i, line in enumerate(md5_generator('original_text.txt'), 1):
+        print('{}) {}'.format(i, line))
 
-    print(summator2(1, 2, 3, 4))
-    print(summator2(1, 2, 3, 4, 5))
-    print(summator2(1, 2, 3, 4, 5, 6))
-    print(summator2(1, 2, 3, 4, 5, 6, 7))
+    print('Результат работы алгоритма SHA-256:')
+    for i, line in enumerate(sha256_generator('original_text.txt'), 1):
+        print('{}) {}'.format(i, line))
