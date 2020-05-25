b8412f7c516b2bd8d753ffb80a57adc24b4cb692
Anvartdinov Timur <AnvartdinovTimurLinux@gmail.com>
2020-05-25 11:28:53 +0300

    Домашнее задание к лекции 1.4 «Decorators» Декоратор с параметрами

--- 1.4/loggers/logger_with_params.py
+++ 1.4/loggers/logger_with_params.py
@@ -2,14 +2,16 @@ from datetime import datetime
 from functools import wraps
 
 
-def logger_with_params(function):
-    @wraps(function)
-    def wrapper(*args, **kwargs):
-        result = function(*args, **kwargs)
+def logger_with_params(path_to_log):
+    def decorator(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            result = function(*args, **kwargs)
 
-        with open('../log.txt', 'a', encoding='utf-8') as f:
-            curent_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            f.write(f'{curent_time}: {function.__name__}(args={args}, kwargs={kwargs}) => {result}\n')
+            with open(path_to_log, 'a', encoding='utf-8') as f:
+                curent_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                f.write(f'{curent_time}: {function.__name__}(args={args}, kwargs={kwargs}) => {result}\n')
 
-        return result
-    return wrapper
+            return result
+        return wrapper
+    return decorator
--- 1.4/main.py
+++ 1.4/main.py
@@ -1,9 +1,13 @@
 import loggers.logger_without_params as logger1
-# from logger_with_params import logger_with_params as logger2
+import loggers.logger_with_params as logger2
 
 
 @logger1.logger_without_params
 def summator(*args):
+    return sum(args)
+
+@logger2.logger_with_params('log2.txt')
+def summator2(*args):
     return sum(args)
 
 
@@ -11,3 +15,8 @@ if __name__ == '__main__':
     print(summator(1, 2, 3, 4))
     print(summator(1, 2, 3, 4, 5))
     print(summator(1, 2, 3, 4, 5, 6))
+
+    print(summator2(1, 2, 3, 4))
+    print(summator2(1, 2, 3, 4, 5))
+    print(summator2(1, 2, 3, 4, 5, 6))
+    print(summator2(1, 2, 3, 4, 5, 6, 7))
